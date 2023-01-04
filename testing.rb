72bdd357c118d79c4f374138b58e0a2f4d6dd487
lana <diegogarciaq98@gmail.com>
2023-01-04 12:20:39 -0500

    Create classroom class and add tests to testing file

--- testing.rb
+++ testing.rb
@@ -1,10 +1,17 @@
 require_relative './person'
+require_relative './student'
+require_relative './classroom'
 require_relative './capitalize_decorator'
 require_relative './trimmer_decorator'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+student1 = Student.new(1, 1, name= 'Yo')
+puts student1
+classroom1 =  Classroom.new("ClassRoom1")
+puts classroom1
+puts classroom1.students
+classroom1.add_student(student1)
+puts classroom1.students
+# classroom -> owner || student -> animals
+
+student2 = Student.new(5, 5, name= 'Tu')
+puts student2.classroom
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,6 @@ require_relative './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.length <= 10 ? @nameable : @nameable.correct_name.slice(0, 10)
+    @nameable.correct_name.length <= 10 ? @nameable : @nameable.correct_name.slice(0, 10) 
   end
 end

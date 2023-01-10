00cd66ae477fdf933419f4ba0ab97c8e6fb39fb9
lana <diegogarciaq98@gmail.com>
2023-01-10 11:14:20 -0500

    Show added books

--- data/run.rb
+++ data/run.rb
@@ -1,12 +1,13 @@
 require 'json'
-json_file = File.open("./data/books.json")
-data_from_file = JSON.load(json_file)
- 
-puts data_from_file["widget"]["debug"]
-
-
-
-
+module ShowBooks
+  def shows
+    json_file = File.open("./data/books.json")
+    # data_from_file = JSON.pretty_generate(json_file)
+    data_from_file = File.read(json_file)
+    puts data_from_file
+    JSON.parse(data_from_file)
+  end
+end
 
 
 # puts data_from_file["widget"]["window"]
--- modules/show_books.rb
+++ modules/show_books.rb
@@ -1,7 +1,11 @@
+require_relative '../data/run'
+
 module ListBooks
+  include ShowBooks
   def show_books
     if Book.all.empty?
       puts 'There\'s no books added yet!'
+      puts shows.length
     else
       Book.all.each do |book|
         puts %(Title: "#{book.title}", Author: #{book.author})

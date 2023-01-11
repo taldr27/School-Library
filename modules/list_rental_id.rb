955c57f7fdcb9fb4f4eb2a9071b920d3a540dd74
lana <diegogarciaq98@gmail.com>
2023-01-11 17:59:04 -0500

    Fix linters errors

--- modules/list_rental_id.rb
+++ modules/list_rental_id.rb
@@ -6,30 +6,8 @@ module ListRentalId
       print 'ID of person: '
       id_selected = gets.chomp.to_i
       @rentals.each do |rental|
-        if rental.person == id_selected
-          puts %(#{rental.date}, Book "#{rental.book.title}" by #{rental.book.author}, rented by #{rental.person.name})
-        else
-          puts 'There\'s no rental with that ID'
-        end
+        puts %(Date: #{rental['date']}, Book "#{rental['title']}" , rented by User ID #{rental['id']}) if rental['person'].to_i == id_selected
       end
     end
   end
 end
-
-# module ListRentalId
-#   def list_rental_id
-#     if @rentals.empty?
-#       puts 'Please add a person and a book first!'
-#     else @rentals.length > 1
-#       print 'ID of person: '
-#       id_selected = gets.chomp.to_i
-#       @rentals.each do |rental|
-#         if rental.person.id == id_selected
-#           puts %(#{rental.date}, Book "#{rental.book.title}" by #{rental.book.author}, rented by #{rental.person.name})
-#         else
-#           puts 'There\'s no rental with that ID'
-#         end
-#       end
-#     end
-#   end
-# end

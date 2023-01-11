7a2023df63a1dd68d7015001de04674461314527
lana <diegogarciaq98@gmail.com>
2023-01-11 17:41:39 -0500

    Fix linters errors pt 1

--- modules/create_person.rb
+++ modules/create_person.rb
@@ -14,13 +14,15 @@ module CreatePerson
         print 'Has parent permission? [Y/N]: '
         permission = gets.chomp
         person = Student.new(age, classroom, name: name, parent_permission: permission)
-        @people.push({ 'id' => person.id, 'age' => person.age, 'classroom' => person.classroom, 'name' => person.name, 'parent_permission' => person.parent_permission })
+        @people.push({ 'id' => person.id, 'age' => person.age, 'classroom' => person.classroom, 'name' => person.name,
+                       'parent_permission' => person.parent_permission })
         puts 'Student created!'
       when 2
         print 'Specialization: '
         specialization = gets.chomp
         person = Teacher.new(age, specialization, name: name)
-        @people.push({ 'id' => person.id, 'age' => person.age, 'specialization' => person.specialization, 'name' => person.name })
+        @people.push({ 'id' => person.id, 'age' => person.age, 'specialization' => person.specialization,
+                       'name' => person.name })
         puts 'Teacher created!'
       end
     else
